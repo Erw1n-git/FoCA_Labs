205a68b2985a1b095a584ace911fd1eda6778a87
erw1nhmtz <erw1nhmtz@gmail.com>
2023-03-25 17:49:16 +0200

    Updated Lab_3/prog1_1 files.

--- Lab_3/prog1_1/main.c
+++ Lab_3/prog1_1/main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int main(int argc, char** argv)
+void readParams(int argc, char** argv)
 {
 	bool isFirst = true;
 	int c;
@@ -18,20 +18,86 @@ int main(int argc, char** argv)
 		switch(c)
 		{
 		case 'h':
-			printf("Usage of %s:\n", argv[0]);
-			printf("\t[-h] Print help page\n");
-			printf("\t[-w \"name\"] Execute file from PATH variable\n");
-			printf("\t[-f \"name\"] Execute file from PATH variable asynchronously\n");
+			param_H(argv);
 			break;
 		case 'w':
-			printf("envoked -w with param: %s\n", optarg);
+			param_W(argv);
 			break;
 		case 'f':
-			printf("envoked -f with param: %s\n", optarg);
+			param_F(argv);
 			break;
 		}
 
 		isFirst = false;
 	}
+}
+
+void param_H(char** argv)
+{
+	printf("Usage of %s:\n", argv[0]);
+	printf("\t[-h] Print help page\n");
+	printf("\t[-w \"name\"] Execute file from PATH variable\n");
+	printf("\t[-f \"name\"] Execute file from PATH variable asynchronously\n");
+}
+
+void param_W(char** argv)
+{
+	pid_t pid;
+	int rv; 
+	switch (pid=fork())
+	{
+	case -1:
+		perror("fork error");
+		exit(1);
+		break;
+	case 0:
+	{
+		printf("Executing file...\n\n");
+		char *prog_name = optarg;
+		execlp(prog_name, prog_name, NULL);
+		printf("Error: exec failed\n");
+        exit(EXIT_FAILURE);
+	}
+	default:
+		wait(&rv);
+		printf("\nThe exit code is: %d\n", rv);
+		printf("Done.\n");
+		break;
+	}
+}
+
+void param_F(char** argv)
+{
+	pid_t pid;
+	pid_t pid2;
+	if((pid = fork()) < 0) exit (-1);
+	if (pid == 0)
+	{
+		if((pid2 = fork()) < 0) exit (-1);
+		if(pid2 == 0)
+		{
+			printf("Executing file asynchronously...\n");
+			char *prog_name = optarg;
+			execlp(prog_name, prog_name, NULL);
+			printf("Error: exec failed\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			printf("Trying to exec child with pid = %d\n", pid2);
+			//printf("Immediately continuing program.\n");
+			exit(0);
+		}
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		printf("Done.\n");
+	}	
+}
+
+int main(int argc, char** argv)
+{
+	readParams(argc, argv);
 	return 0;
 }
--- Lab_3/prog1_1/main.h
+++ Lab_3/prog1_1/main.h
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void readParams(int argc, char** argv);
+
+void param_H(char** argv);
+void param_W(char** argv);
+void param_F(char** argv);
